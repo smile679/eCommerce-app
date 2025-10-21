362844eb271eae5edd4e51fbd3cbb6ad85b007ef
samuel88 <samuelgide88@gmail.com>
2025-10-22 01:21:19 +0300

    Refactor order controller: standardize response format and improve error handling

--- Client/src/components/admin-view/Orders.jsx
+++ Client/src/components/admin-view/Orders.jsx
@@ -66,7 +66,6 @@ function AdminOrdersView() {
               )
               )
             }
-             
           </TableBody>
         </Table>
       </CardContent>
--- Server/controllers/admin/order-controller.js
+++ Server/controllers/admin/order-controller.js
@@ -1,53 +1,80 @@
-const Order = require("../../models/Order")
+const Order = require("../../models/Order");
 
-
-const getAllOrdersOfAllUsers = async(req,res)=>{
-  try{
-    const orders = await Order.find({})
-    if(!orders.length){
+const getAllOrdersOfAllUsers = async (req, res) => {
+  try {
+    const orders = await Order.find({});
+    if (!orders.length) {
       return res.status(404).json({
-        success : 'false',
-        message : 'No order found!'
-      })
+        success: "false",
+        message: "No order found!",
+      });
     }
 
     res.status(200).json({
-      success : true,
-      data : orders,
-    })
-
-  }catch(error){
-    console.log(error)
+      success: true,
+      data: orders,
+    });
+  } catch (error) {
+    console.log(error);
     res.status(500).json({
       success: false,
       message: "some error occured!",
-    })
+    });
   }
-}
+};
 
-const getOrderDetailsForAdmin = async(req,res)=>{
-  try{
+const getOrderDetailsForAdmin = async (req, res) => {
+  try {
     const { id } = req.params;
-    const order = await Order.findById(id)
-    if(!order){
+    const order = await Order.findById(id);
+    if (!order) {
       return res.status(404).json({
-        success : 'false',
-        message : 'Order Not found!'
-      })
+        success: "false",
+        message: "Order Not found!",
+      });
     }
 
     res.status(200).json({
-      success : true,
-      data : order,
-    })
-
-  }catch(error){
-    console.log(error)
+      success: true,
+      data: order,
+    });
+  } catch (error) {
+    console.log(error);
     res.status(500).json({
       success: false,
       message: "some error occured!",
-    })
+    });
   }
-}
+};
 
-module.exports = { getAllOrdersOfAllUsers, getOrderDetailsForAdmin };
+const updateOrderStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { orderStatus } = req.body;
+
+    const order = await Order.findByIdAndUpdate(id, { orderStatus });
+    if (!order) {
+      return res.status(404).json({
+        success: "false",
+        message: "Order Not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Order status is updated successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "some error occured!",
+    });
+  }
+};
+
+module.exports = {
+  getAllOrdersOfAllUsers,
+  getOrderDetailsForAdmin,
+  updateOrderStatus,
+};
