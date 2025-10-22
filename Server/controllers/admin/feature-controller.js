2a0fc169207a630510d6cb583e8feb7e185033ed
samuel88 <samuelgide88@gmail.com>
2025-10-23 02:18:02 +0300

    feature

---
+++ Server/controllers/admin/feature-controller.js
@@ -0,0 +1,26 @@
+
+
+
+const addFeatureImage = (req, res)=>{
+  try{
+
+  }catch(e){
+    console.log(e);
+    res.json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+}
+
+const getFeatureImages = (req, res)=>{
+  try{
+
+  }catch(e){
+    console.log(e);
+    res.json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+}
